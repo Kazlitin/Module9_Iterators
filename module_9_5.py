8927b8d7a207471d4650c5f1052b504e852e390b
Kazlitin <167462725+Kazlitin@users.noreply.github.com>
2024-07-20 15:57:12 +0300

    Add files via upload

--- module_9_5.py
+++ module_9_5.py
@@ -1,6 +1,5 @@
 class StepValueError(ValueError):
-    def __init__(self, message):
-        super().__init__(message)
+    pass
 
 class Iterator:
     def __init__(self, start, stop, step=1):
@@ -14,9 +13,9 @@ class Iterator:
         return self
 
     def __next__(self):
-        if self.step == 0 or (self.step < 0 and (self.start >= self.stop and self.start < self.stop + self.step)):
-            raise StepValueError("Шаг не может быть равен 0 или отрицательным и меньше нуля при start >= stop")
-        elif self.pointer > self.stop:
+        if self.step == 0:
+            raise StepValueError("Шаг не может быть равен 0")
+        elif self.pointer >= self.stop:
             raise StopIteration
         else:
             result = self.pointer
@@ -33,6 +32,7 @@ except StepValueError:
 iter2 = Iterator(-5, 1)
 iter3 = Iterator(6, 15, 2)
 iter4 = Iterator(5, 1, -1)
+iter5 = Iterator(10, 1)
 
 for i in iter2:
     print(i, end=' ')
@@ -41,5 +41,8 @@ for i in iter3:
     print(i, end=' ')
 print()
 for i in iter4:
+    print(i, end=' ')
+print()
+for i in iter5:
     print(i, end=' ')
 print()
